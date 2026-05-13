5ea16aa8f705fd094fe5961a28292b09a5167763
leoseis <eboscrib@yahoo.com>
2026-05-13 09:57:54 +0100

    upated api.ts with token

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://192.168.43.207:8000/api",
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("access");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
---
+++ src/api/auth.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "./api";
+
+export const loginUser = async (username: string, password: string) => {
+  const response = await api.post("/auth/login/", {
+    username,
+    password,
+  });
+
+  const data = response.data;
+
+  await AsyncStorage.setItem("access", data.access);
+  await AsyncStorage.setItem("refresh", data.refresh);
+
+  return data;
+};
