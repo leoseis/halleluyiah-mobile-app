9b7a11bafef9c85168def4d7fcf1f295bad45878
leoseis <eboscrib@yahoo.com>
2026-05-14 13:12:58 +0100

    [200~Signup Screen Login Screen Store JWT token Auto-authenticated requests~

---
+++ app/register.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Alert, Button, TextInput, View } from "react-native";
+
+import { BASE_URL } from "../src/api/services/api";
+export default function RegisterScreen() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/auth/register/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        Alert.alert("Success", "Account created");
+      } else {
+        Alert.alert("Error", JSON.stringify(data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={{ padding: 20 }}>
+      <TextInput placeholder="Username" onChangeText={setUsername} />
+
+      <TextInput placeholder="Email" onChangeText={setEmail} />
+
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button title="Register" onPress={register} />
+    </View>
+  );
+}
