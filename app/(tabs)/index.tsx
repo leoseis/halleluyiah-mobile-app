331de8e0b5925cf75f9ee015f25fe885bd66611c
leoseis <eboscrib@yahoo.com>
2026-06-26 12:47:29 +0100

    added aa a staic polished header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -163,6 +163,46 @@ export default function HomeScreen() {
         backgroundColor: theme.background,
       }}
     >
+      <View
+        style={{
+          backgroundColor: "#001f5b",
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+          paddingHorizontal: 20,
+          paddingTop: 50,
+          paddingBottom: 30,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 18,
+          }}
+        >
+          👋 Good Morning,
+        </Text>
+
+        <Text
+          style={{
+            color: "white",
+            fontSize: 28,
+            fontWeight: "bold",
+            marginTop: 5,
+          }}
+        >
+          Leonard
+        </Text>
+
+        <Text
+          style={{
+            color: "#dbeafe",
+            marginTop: 8,
+            fontSize: 15,
+          }}
+        >
+          Welcome to RCCG Hallelujah Parish
+        </Text>
+      </View>
       <FlatList
         data={filteredAnnouncements}
         refreshing={refreshing}
